cf9b1c12d64be1d91cd9de91e0a229a28e922d7f
jsoos <pr75505254@zegelipae.pe>
2023-04-27 20:49:08 -0500

    SEMANA DOS

--- LOGRO01.py
+++ LOGRO01.py
@@ -1,1 +1,22 @@
-print("+" + 10 * "-" + "+")
+
+
+# ---SEMANA DOS---
+
+#  CARÁCTERES ESPECIALES
+print("I'm\nlearning \n"'"Python"')
+
+print("--------")
+# PARÁMETRO END
+print("Hola", end=" ")
+print("Adios")
+
+print("--------")
+# TRANSFERENCIA
+print('Un tipo le dice a otro : "¿Cómo estás?"')
+print("Y el otro le contesta : '¡Pues anda que tú!'")
+
+print("--------")
+text1 = "Hola"
+text2 = "Mundo"
+text3 = "Python"
+print(text1, text2, text3, sep="-")
