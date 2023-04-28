3b27b0e1925fd7c4d369923da21849aaaab451f9
jsoos <pr75505254@zegelipae.pe>
2023-04-28 01:45:36 -0500

    TERMINADO 01

---
+++ ACTIVIDADES.py
@@ -0,0 +1,39 @@
+# NOMBRE , EDAD Y MOSTRAR
+nombre = input("Ingrese su nombre: ")
+edad = int(input("Ingrese su edad: "))
+print("Su nombre es: ", nombre)
+print("Su edad es: ", edad)
+print("--------")
+#-----------------
+
+# DOS NÚMEROS ENTEROS, SUMAR Y MOSTRAR
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+suma = num1 + num2
+print("La suma de los dos números es: ", suma)
+print("--------")
+#-----------------
+
+# NOMBRE DE PRODUCTO, PRECIO, CUANTO VA A PAGAR Y MOSTRAR VUELTO
+producto = input("Ingrese el nombre del producto: ")
+precio = float(input("Ingrese el precio del producto: "))
+dineropago = float(input("Ingrese el dinero con el que va a pagar: "))
+vuelto = dineropago - precio
+print("El nombre del producto es: ", producto)
+print("El precio del producto es: ", precio)
+print("El dinero con el que va a pagar es: ", dineropago)
+print("El vuelto es: ", vuelto)
+print("--------")
+#-----------------
+
+# SUMAR AHORROS, MOSTRAR AHORROS Y MOSTRAR TOTAL DE 5 MESES
+ahorro1 = float(input("Ingrese el ahorro del Enero: "))
+ahorro2 = float(input("Ingrese el ahorro del Febrero: "))
+ahorro3 = float(input("Ingrese el ahorro del Marzo: "))
+ahorro4 = float(input("Ingrese el ahorro del Abril: "))
+ahorro5 = float(input("Ingrese el ahorro del Mayo: "))
+total = ahorro1 + ahorro2 + ahorro3 + ahorro4 + ahorro5
+print("El total de los 5 meses es: ", total)
+print("--------")
+#-----------------
+
